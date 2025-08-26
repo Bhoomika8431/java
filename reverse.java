8baeb78cc9693ac85b712dbffca97964efe4685f
Bhoomika. <bhoomikulal735@gmail.com>
2025-08-26 21:12:52 +0530

    added new files

---
+++ reverse.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class reverse {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int num=in.nextInt();
+        
+        //int num=9876;
+        int rev=0;
+        while(num>0){
+           int rem=num%10;
+            rev=rev*10+rem;
+            num=num/10;
+        }
+        System.out.println(rev);
+        }
+}
