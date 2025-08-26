5916374462b61502904165da3b4daf2316707ae7
Bhoomika. <bhoomikulal735@gmail.com>
2025-08-26 11:57:09 +0530

    updated code

--- Fibo.java
+++ Fibo.java
@@ -6,12 +6,12 @@ public class Fibo {
         int n=in.nextInt();
         int a=0;
         int b=1;
-        int count=2;
-        while(count<=n){
+        int c=2;
+        while(c<=n){
             int temp=b;
-            b=b+a;
+            b=a+b;
             a=temp;
-            count++;
+            c++;
         }
         System.out.println(b);
     }
